57c554f56f30cec48ab40979e5238e9760fbadd7
emmadumbi <dumbiemmanuel29@gmail.com>
2025-10-18 00:58:25 +0000

    create cart modal

--- script.js
+++ script.js
@@ -1,23 +1,85 @@
 const increaseBtn = document.querySelector('.increase-btn');
 const quantityEl = document.querySelector('.quantity-el');
 const decreaseBtn = document.querySelector('.decrease-btn');
+const cartQuantityEl = document.querySelector('.cart-quantity-el');
+const addToCartBtn = document.querySelector('.add-to-cart-btn');
+const checkOutCartQuantity = document.querySelector('.checkout-cart-quantity');
+const totalPrice = document.querySelector('.total-price');
+const emptyCart = document.querySelector('.empty-cart-container');
+const filledCart = document.querySelector('.filled-cart-container');
+const cartIcon = document.querySelector('.cart-icon');
+const deleteCart = document.querySelector('.delete-cart');
 
-
-let initialQuantity = 0;
-
-function updateQuantityEl() {
-  quantityEl.textContent = initialQuantity;
-}
-updateQuantityEl();
-
-increaseBtn.addEventListener('click', () => {
-  initialQuantity++;
-  updateQuantityEl();
-});
-
-decreaseBtn.addEventListener('click', ()=>{
-  if(!initialQuantity <= 0){
-    initialQuantity--;
-    updateQuantityEl();
+function renderPage() {
+  let cartQuantity = Number(localStorage.getItem('cartQuantity')) || 0;
+  
+  let initialQuantity = 0;
+  
+  filledCart.style.display = 'none'
+  emptyCart.style.display = 'none'
+  if (cartQuantity === 0) {
+    cartQuantityEl.style.display = 'none'
   }
-})
+  
+  function updateQuantityEl() {
+    quantityEl.textContent = initialQuantity;
+  }
+  updateQuantityEl();
+  
+  function updateCartQuantityEl() {
+    cartQuantityEl.textContent = cartQuantity;
+    checkOutCartQuantity.textContent = cartQuantity;
+  };
+  updateCartQuantityEl();
+  
+  function addToCart() {
+    cartQuantity += initialQuantity;
+    updateCartQuantityEl();
+  };
+  
+  function calculateTotal() {
+    let productPrice = 125;
+    const total = productPrice * cartQuantity;
+    totalPrice.textContent = `$${total}.00`
+  };
+  calculateTotal();
+  
+  function saveToStorage() {
+    localStorage.setItem('cartQuantity', String(cartQuantity));
+  }
+  
+  increaseBtn.addEventListener('click', () => {
+    initialQuantity++;
+    updateQuantityEl();
+  });
+  
+  decreaseBtn.addEventListener('click', () => {
+    if (!initialQuantity <= 0) {
+      initialQuantity--;
+      updateQuantityEl();
+    }
+  });
+  
+  cartIcon.addEventListener('click', () => {
+    if (cartQuantity === 0) {
+      emptyCart.style.display = 'block'
+    } else if (cartQuantity > 0) {
+      filledCart.style.display = 'block'
+    }
+  });
+  
+  addToCartBtn.addEventListener('click', () => {
+    addToCart();
+    calculateTotal();
+    saveToStorage();
+    cartQuantityEl.style.display = 'block'
+  });
+  
+  deleteCart.addEventListener('click',()=>{
+    cartQuantity = 0;
+    localStorage.removeItem('cartQuantity');
+    renderPage();
+    
+  })
+};
+renderPage();
